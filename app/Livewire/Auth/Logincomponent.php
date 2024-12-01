7f1921b0ae178ce2fd354ac1b94fa26db6ffa521
Dike Wisdom <dikewisdom787@gmail.com>
2024-12-01 02:26:30 -0500

    Add Livewire support for user authentication with LoginComponent

---
+++ app/Livewire/Auth/Logincomponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Component;
+
+class Logincomponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.logincomponent');
+    }
+}
