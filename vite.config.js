10341124e946dc8c273fe824ef20018accb303d1
Dike Wisdom <dikewisdom787@gmail.com>
2024-12-15 16:22:37 -0500

    Add polling configuration to Vite server for improved file watching

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import laravel from "laravel-vite-plugin";
 import "dotenv/config";
 
 // Vite configuration
 export default defineConfig({
-  root: '.',  // Ensure the root is correctly pointing to the Laravel root directory
-  plugins: [
-    laravel({
-      input: [
-        "resources/css/app.css",
-        "resources/js/app.js", // Main JS entry (if needed)
-        "resources/js/react/app.jsx", // Main React entry
-        "resources/js/chat.jsx", // Chat entry specifically
-        "resources/js/comps/*.jsx", // Chat entry specifically
-      ],
-      refresh: true, // This will allow Vite to refresh automatically on changes
-    }),
-  ],
+    root: '.',  // Ensure the root is correctly pointing to the Laravel root directory
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 500,
+        }
+    }, plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js", // Main JS entry (if needed)
+                "resources/js/react/app.jsx", // Main React entry
+                "resources/js/chat.jsx", // Chat entry specifically
+                "resources/js/comps/*.jsx", // Chat entry specifically
+            ],
+            refresh: true, // This will allow Vite to refresh automatically on changes
+        }),
+    ],
 });
