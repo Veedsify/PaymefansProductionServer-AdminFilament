509e390cdb9101f46aa23b3624691d3df78afb5b
Dike Wisdom <dikewisdom787@gmail.com>
2025-01-06 00:31:11 +0100

    Enhance ProductResource form with detailed product information fields and category selection

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -5,7 +5,10 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\ProductSize;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -20,19 +23,41 @@ class ProductResource extends Resource
     protected static ?string $navigationIcon = 'hugeicons-shirt-01';
     protected static ?string $navigationGroup = 'Products';
     protected static ?int $navigationSort = -2;
-    
+
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                
+                Section::make('Product Information')
+                    ->description('Enter the product details')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Name')
+                            ->required(),
+                        Forms\Components\Textarea::make('description')
+                            ->label('Description')
+                            ->required(),
+                        Forms\Components\TextInput::make('price')
+                            ->label('Price')
+                            ->required(),
+                        Forms\Components\TextInput::make('instock')
+                            ->label('In Stock')
+                            ->required(),
+                        Forms\Components\Select::make('category_id')
+                            ->options(function () {
+                                return ProductCategory::all()->pluck('name', 'id');
+                            })
+                            ->label('Category')
+                            ->required(),
+                    ])
             ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable()
