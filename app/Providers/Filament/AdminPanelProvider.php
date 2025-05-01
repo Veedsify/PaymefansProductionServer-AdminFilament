cf8a75720c01d99a41c66c9c23af78a1992a45fb
= <=>
2025-05-01 10:27:44 +0100

    Implement code changes to enhance functionality and improve performance

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id("admin")
             ->path("admin")
-            ->font("Rubik")
+            ->font("Inter_Tight")
             ->globalSearchKeyBindings(["command+k", "ctrl+k"])
             ->colors([
                 "primary" => Color::Cyan,
