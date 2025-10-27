cf9e5a3e3fdc037ff43330976e2eb9e7093d9983
= <=>
2025-10-27 16:05:34 +0100

    fix: Correct visibility setting for group icon in GroupsResource

--- app/Filament/Resources/GroupsResource.php
+++ app/Filament/Resources/GroupsResource.php
@@ -47,7 +47,7 @@ class GroupsResource extends Resource
                         ->label("Group Icon")
                         ->directory("groups/icon")
                         ->disk("s3")
-                        ->visibility("publico")
+                    ->visibility("public")
                         ->image(),
 
                     Forms\Components\Toggle::make("isActive")
